f9ac7273e5320c83c868d57bc5ef1adeefacba1d
Rober2020 <rrsanchez8@hotmail.com>
2021-05-19 22:02:27 -0400

    agrego modelo Character y pongo su controler y json en el enum para verlo desde el swagger

---
+++ WebApi/Controllers/CharacterController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CharacterController : ControllerBase
+    {
+        private static Character knight = new Character();
+
+        [HttpGet]
+        public ActionResult<Character> Get()
+        {
+            return Ok(knight);
+        }
+    }
+}
